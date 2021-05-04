cea7983cc7f842b96bc3bdcd7df7f0c05f0a0675
Joseph Dubon <josephdubon@pm.me>
2021-05-03 21:03:56 -0700

    Updated post list view to class-based view using django generic view ListView.

--- blog/views.py
+++ blog/views.py
@@ -1,38 +1,23 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 
 # List posts view
-def post_list(request):
-    object_list = Post.published.all()
-    # Instantiate the Paginator class with the number of
-    # objects that you want to display on each page.
-    paginator = Paginator(object_list, 3)
-    # Get the page GET parameter, which indicates the
-    # current page number.
-    page = request.GET.get('page')
-    # Obtain the objects for the desired page by
-    # calling the page() method of Paginator.
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # If the page parameter is not an integer, you retrieve
-        # the first page of results.
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If this parameter is a number higher than the last page
-        # of results, you retrieve the last page.
-        posts = paginator.page(paginator.num_pages)
-        # Pass the page number and retrieved objects to the template.
-    return render(
-        request,
-        'blog/post/list.html',
-        {
-            'page': page,
-            'posts': posts,
-        },
-    )
+class PostListView(ListView):
+    # Use a specific QuerySet instead of retrieving all objects.
+    # - Instead of defining a queryset attribute, you could have specified model = Post
+    # - and Django would have built the generic Post.objects.all() QuerySet for you.
+    queryset = Post.published.all()
+    # Use the context variable posts for the query results.
+    # - The default variable is object_list if you don't specify any context_object_name
+    context_object_name = 'posts'
+    # Paginate the result, displaying three objects per page.
+    paginate_by = 3
+    # Use a custom template to render the page. If you don't set a default
+    # template, ListView will use blog/post_list.html
+    template_name = 'blog/post/list.html'
 
 
 # Detail post view
