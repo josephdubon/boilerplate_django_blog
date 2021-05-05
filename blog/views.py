baa3277814da752968d0b16e296940217173e371
Joseph Dubon <josephdubon@pm.me>
2021-05-04 17:04:33 -0700

    Added functionality to share post through email

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import send_mail
+
 from .models import Post
 from .forms import EmailPostForm
 
@@ -41,16 +43,28 @@ def post_detail(request, year, month, day, post):
 def post_share(request, post_id):
     # Retrieve post by id
     post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
     if request.method == 'POST':
         # If form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
             # If form fields were validated
             cd = form.cleaned_data
-            # ... TODO: send email here
+            # Retrieve the absolute path of the post using its get_absolute_url() method.
+            # - You use this path as an input for request.build_absolute_uri() to build a
+            # - complete URL, including the HTTP schema and hostname.
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            # Send the email to the email address contained in the to field of the form
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
+
     return render(request, 'blog/post/share.html', {
         'post': post,
         'form': form,
+        'sent': sent
     })
