a2a804b1d95d206342a8d050f0e96dfb0d99a037
Joseph Dubon <josephdubon@pm.me>
2021-05-05 17:48:17 -0700

    Added model for Comment functionality. Implemented 'active' boolean field to manage  unwanted comments.

--- blog/models.py
+++ blog/models.py
@@ -66,3 +66,22 @@ class Post(models.Model):
                 self.slug,
             ]
         )
+
+
+# Comment model
+class Comment(models.Model):
+    # ForeignKey to associate a comment with a single post.
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    # Boolean field that you will use to manually deactivate inappropriate/unwanted comments.
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
