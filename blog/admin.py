9ca1f3ce6d94ceffbaa2ce6944b67a9d575e9f48
Joseph Dubon <josephdubon@pm.me>
2021-05-05 17:50:51 -0700

    Added the comment model to the admin pane with custom list_display, list_filter, search_filter.

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 # The @admin.register() decorator performs the same
@@ -48,3 +48,10 @@ class PostAdmin(admin.ModelAdmin):
     # You can also see that the posts are ordered by STATUS and PUBLISH
     # - columns by default.
     ordering = ('status', 'publish')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active',)
+    list_filter = ('active', 'created', 'updated',)
+    search_fields = ('name', 'email', 'body',)
