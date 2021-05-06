6064ef376d3eefbc5e05a18a331bf9a2bffca90b
Joseph Dubon <josephdubon@pm.me>
2021-05-06 11:57:39 -0700

    Added 'tags' field to Comment model.

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 
 # Published custom post manager model
@@ -47,6 +48,8 @@ class Post(models.Model):
     objects = models.Manager()
     # Custom manager
     published = PublishedManager()
+    # Add the taggit TaggableManager for tag functionality
+    tags = TaggableManager()
 
     class Meta:
         ordering = ('-publish',)
